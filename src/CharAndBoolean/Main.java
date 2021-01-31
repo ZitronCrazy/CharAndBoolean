d7d87822c495e5853b66689da643cc49a56bbf68
ZitronCrazy <zitro5@protonmail.com>
2021-01-31 01:10:00 +0100

    CharAndBoolean

---
+++ src/CharAndBoolean/Main.java
@@ -0,0 +1,25 @@
+package CharAndBoolean;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char mySecondUnicodeChar = '\u058D';
+        char myAppleHeartChar = '\u2766';
+        char myHeartChar = '\u2665';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(mySecondUnicodeChar);
+        System.out.println(myAppleHeartChar);
+        System.out.println(myHeartChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+
+}
